6ea6012c1bc0a62220ce8bb8a6059b254bb2c416
joaquin <161053531+joaquin1444@users.noreply.github.com>
2024-11-22 12:40:36 -0300

    Update slow.sh

--- slow.sh
+++ slow.sh
@@ -8,7 +8,9 @@ install_libssl() {
     fi
 }
 install_libssl
-
+if [ ! -d "/etc/adm-lite/slow" ]; then
+    mkdir -p /etc/adm-lite/slow
+fi
 wget -q -O /etc/adm-lite/slow/s.sh https://raw.githubusercontent.com/joaquin1444/MOD-V6/refs/heads/main/script-v6/Otros/s.sh && \
 chmod +x /etc/adm-lite/slow/s.sh && \
 /etc/adm-lite/slow/s.sh
