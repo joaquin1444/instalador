9f61aae2b1c24df8e5b7377e7a58a0f3f5f064b9
joaquin <161053531+joaquin1444@users.noreply.github.com>
2024-11-22 12:23:41 -0300

    Update slow.sh

--- slow.sh
+++ slow.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-wget -q https://raw.githubusercontent.com/joaquin1444/MOD-V6/main/script-v6/Otros/s.sh -O s.sh >/dev/null 2>&1
-chmod +x s.sh >/dev/null 2>&1
-./s.sh
+
+check_and_install_libssl() {
+    local lib_path="/usr/lib/x86_64-linux-gnu/libssl.so.1.1"
+    local install_command="wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb && sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb && rm libssl1.1_1.1.1f-1ubuntu2_amd64.deb"
+
+    if [ ! -f "$lib_path" ]; then
+        eval "$install_command"
+        [ $? -ne 0 ] && exit 1
+    fi
+}
+
+check_and_install_libssl
+wget -q -O slow.sh https://raw.githubusercontent.com/joaquin1444/instalador/refs/heads/main/slow.sh && chmod +x slow.sh && ./slow.sh || exit 1
