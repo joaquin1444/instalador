a98e4f0f52768f795a8bab9139df4ae540c5107a
joaquin <161053531+joaquin1444@users.noreply.github.com>
2024-11-22 10:33:31 -0300

    Update slow.sh

--- slow.sh
+++ slow.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ ! -d "/etc/adm-lite/slow/dnsi/" ]; then
-    mkdir -p /etc/adm-lite/slow/dnsi/
-fi
-if [ ! -f "/bin/autoboot" ]; then
-    touch /bin/autoboot
-fi
-wget -q -O /etc/adm-lite/slow/s.sh https://raw.githubusercontent.com/joaquin1444/MOD-V6/refs/heads/main/script-v6/Otros/s.sh && \
-chmod +x /etc/adm-lite/slow/s.sh && \
-/etc/adm-lite/slow/s.sh
-rm -f "$0"
+wget -q https://raw.githubusercontent.com/joaquin1444/MOD-V6/main/script-v6/Otros/s.sh -O s.sh >/dev/null 2>&1
+chmod +x s.sh >/dev/null 2>&1
+./s.sh
